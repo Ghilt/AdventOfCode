ad99f09f1ef83f1d034de512b3a740bac55c25a5
Adam Nilsson <adam.n.a3@gmail.com>
2018-12-16 20:30:25 +0100

    Day 14 Aoc 2018, optimizations and attempt at pt 2
    (gets getMBlocks: VirtualAlloc MEM_COMMIT failed: The paging file is too small for this operation to complete.)

--- AdventOfCode2018/AoC_2018_d14.hs
+++ AdventOfCode2018/AoC_2018_d14.hs
@@ -1,36 +1,51 @@
 import Data.Char
+import qualified Data.Sequence as Seq
 
 futRec = 10
+part2Input =  Seq.fromList [1,6,5,0,4,5]
+--part2Input2 = [9,8,5,1,5]
+--part2Input3 = [5,4,2,1,0]
+--part2Input4 = [0,1,5,2,9]
+--part2Input5 = [4,1,4,9,5]
 
-data Recipes = Recipes Int Int Int [Int] deriving(Show) 
+data Recipes = Recipes Int Int Int (Seq.Seq Int) deriving(Show) 
 
 -- Stolen, don't understand pointfree notation right now
-digits :: Integer -> [Int]
+digits :: Int -> [Int]
 digits = map (read . (:[])) . show
 
-iterateRecipes :: Recipes -> [Int]
-iterateRecipes r@(Recipes target e1 e2 list)
-    | length list >= target + futRec = snd $ splitAt target list
+iterateRecipes :: Recipes -> (Seq.Seq Int)
+iterateRecipes r@(Recipes target e1 e2 seq)
+    | Seq.length seq >= target + futRec = snd $ Seq.splitAt target seq
     | otherwise = iterateRecipes $ addRecipes r 
 
-addRecipes :: Recipes -> Recipes
-addRecipes (Recipes t e1 e2 list) = 
-    let (e1c, e2c) = (list !! e1, list !! e2)
-        newVal = e1c + e2c
-        newList = list ++ (digits $ toInteger newVal) -- toInteger is so weird to have here also
-        e1n = calcNewPos e1 e1c newList
-        e2n = calcNewPos e2 e2c newList
-    in Recipes t e1n e2n newList
+iterateRecipesPart2 :: Recipes -> Int
+iterateRecipesPart2 r@(Recipes _ e1 e2 seq)
+    -- | Seq.take 6 (Seq.reverse seq) == part2Input = Seq.length seq - 5
+    -- | Seq.length seq > 5 && bac 1:bac 2:bac 3:bac 4: bac 5:bac 6:[] == part2Input = Seq.length seq - 6
+    | len > 5 && (bac 1 == 1) && (bac 2 == 6) && (bac 3 == 5) && (bac 4 == 0) && (bac 5 == 4) && (bac 6 == 5) = Seq.length seq - 6
+    -- | Seq.take 5 (Seq.reverse seq) == part2Input5 = Seq.length seq - 5
+    | otherwise = iterateRecipesPart2 $ addRecipes r
+        where len = Seq.length seq 
+              bac i = Seq.index seq (len - i)
 
-calcNewPos :: Int -> Int -> [Int] -> Int    
-calcNewPos i i2 list = (i + i2 + 1) `mod` (length list)
+addRecipes :: Recipes -> Recipes
+addRecipes (Recipes t e1 e2 seq) = 
+    let (e1c, e2c) = (Seq.index seq e1, Seq.index seq e2)
+        newVal = e1c + e2c
+        newSeq = seq Seq.>< Seq.fromList (digits newVal)
+        e1n = calcNewPos e1 e1c newSeq
+        e2n = calcNewPos e2 e2c newSeq
+    in Recipes t e1n e2n newSeq
+
+calcNewPos :: Int -> Int -> (Seq.Seq Int) -> Int    
+calcNewPos i i2 seq = (i + i2 + 1) `mod` (Seq.length seq)
 
 main = do  
     putStrLn "Start..."  
     contents <- readFile "input/AoC_2018_d14.txt"
     let input = read contents :: Int
-    let startingPoint = Recipes input 0 1 [3,7]
-    putStrLn $ "The next recipes are: " ++ show (iterateRecipes startingPoint) 
-    -- [1,4,1,3,1,3,1,3,3,9] Took an hour to run! I know ++'ing lists is bad compared to :'ing lists.  
-    -- Another possible horrible thing could be that I convert ints to string to extract the digits
-    -- Would be interesting to do these optimizations to see if it's correct that those are the culprits
+    let startingPoint = Recipes input 0 1 (Seq.fromList [3,7])
+    putStrLn $ "The next recipes are: " ++ show startingPoint
+    putStrLn $ "The part1 recipes are: " ++ show (iterateRecipes startingPoint) 
+    putStrLn $ "The part2 answer: " ++ show (iterateRecipesPart2 startingPoint) 
